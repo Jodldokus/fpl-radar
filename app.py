eca89a299136651395e37afe531eea35657be382
Friedrich Panse <friedrichpanse@localhost.localdomain>
2021-04-13 20:09:29 +0200

    added mobile css hopefully

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from os import environ
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL').replace("://", "ql://", 1) or 'sqlite:///myDB.db'
+if environ.get('DATABASE_URL') != None:
+    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL').replace("://", "ql://", 1) or 'sqlite:///myDB.db'
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db'
 app.config['SECRET_KEY'] = 'MEGAsecret'
 
 db = SQLAlchemy(app)
