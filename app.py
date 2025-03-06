c268fedf8cc3823dd3e83084d2424b3313ee165a
Jodldokus <friedrich.panse@gmail.com>
2025-03-06 12:04:39 +0100

    change how to get sqlite file

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import environ
+from os import environ, path
+from dotenv import load_dotenv
 
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, ".env"))
 
 app = Flask(__name__)
-if environ.get("DATABASE_URL") != None:
-    app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DATABASE_URL").replace(
-        "://", "ql://", 1
-    )
-else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///myDB.db"
+
+sqlite_url = environ.get("SQLITE_URL")
+
+if not sqlite_url:
+    raise ValueError("No SQLITE_URL set for Flask application")
+
+db_path = path.join(basedir, sqlite_url, "myDB.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_path
+
 app.config["SECRET_KEY"] = "MEGAsecret"
 
 db = SQLAlchemy(app)
